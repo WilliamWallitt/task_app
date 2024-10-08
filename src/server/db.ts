36945f3d849f68c7cc87683e9cdd144aa0fd0938
WilliamWallitt <willcdswallitt@gmail.com>
2024-10-08 13:43:15 +0100

    undo db changes

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,22 +1,15 @@
-import { PrismaClient as SQLiteClient } from '../../prisma/generated/client1'; // Import SQLite Prisma client
-import { PrismaClient as MySQLClient } from '../../prisma/generated/client2'; // Import MySQL Prisma client
+import { PrismaClient } from '@prisma/client';
 
 import { env } from "~/env";
 
-const createPrismaClient = () => {
-    // const isProduction = env.NODE_ENV === "production";
-    const isProduction = env.DATABASE_URL !== 'file:./dev.db'
-
-    // Use MySQL client in production, SQLite client in development
-    return isProduction ? new MySQLClient({
-        log: ["error"], // Log errors in production
-    }) : new SQLiteClient({
-        log: ["query", "error", "warn"], // Log queries, errors, and warnings in development
+const createPrismaClient = () =>
+    new PrismaClient({
+        log:
+            env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
     });
-};
 
 const globalForPrisma = globalThis as unknown as {
-    prisma: SQLiteClient | MySQLClient | undefined;
+    prisma: ReturnType<typeof createPrismaClient> | undefined;
 };
 
 export const db = globalForPrisma.prisma ?? createPrismaClient();
